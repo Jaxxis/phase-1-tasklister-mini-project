f162ca431ecb1460fb6cddf8b8a9ea97484bc7a6
Jake Mckenzie-Edwards <jake.mckenzieedwards@gmail.com>
2023-04-20 10:50:38 +1000

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDoList(e.target.new_task_description.value)
+    form.reset()
+  })
+
+
 });
+
+function addToDoList(todo){
+  const li = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', handleDelete)
+  li.textContent = `${todo}  `
+  li.appendChild(btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
